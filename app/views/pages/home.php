76c1012ebf6aa0b36ffd0e425c33e30310393b4a
DrgFlorin <bayat.bun@gmail.com>
2018-08-02 01:32:39 -0700

    Added preloader

--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -11,9 +11,22 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <base href="../public/assets/">
     <link rel="icon" href="img/<?php echo $tab_icon;?>">
+    <link rel="stylesheet" href="style/main.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <script>
+        $(window).on('load', function() {
+            $('.preloader').addClass('complete');
+            $('.loader').addClass('end');
+        })
+    </script>
+    
     <title> <?php echo $title;?></title>
 </head>
 <body>
-    <img src="img/<?php echo $tab_icon; ?>">
+    <div class="preloader">
+        <div class="loader"></div>
+    </div>
+    <h1>Loaded</h1>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/aUvfzHHTKJU?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 </body>
 </html>
